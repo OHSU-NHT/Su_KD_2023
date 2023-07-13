f59fcdc879e4b8d8f91f1008322358d2519790e4
OHSU-NHT <121542320+OHSU-NHT@users.noreply.github.com>
2023-07-13 03:22:57 -0700

    Add files via upload

--- Figures/Fig.3.R
+++ Figures/Fig.3.R
@@ -4,150 +4,27 @@ library(dplyr)
 library(here)
 library(ggplot2)
 library(RColorBrewer)
-library(monocle3)
-library(SeuratWrappers)
-library(Signac)
-library(patchwork)
-library(Matrix)
+library(ggpubr)
+library(tibble)
 
-# Fig.3D ---------------------------------------------------------------------------------------
-SO <- readRDS(here("CONTROL_DCT.rds"))
+# Fig. 7C ------------------------------------------------------------------------------------------------
+SO <- readRDS(here("ALL_DCT.rds"))
 Idents(SO) <- "subclass.l1"
+my_levels <- c("DCT1", "DCT2", "Prolif")
+Idents(SO) <- factor(x = Idents(SO), levels = my_levels)
 
 mycols.DCT <- c("#66c2a5", # DCT1
-                         "#fc8d62", # DCT2
-                         "#A91554") # Prolif
+                "#fc8d62", # DCT2
+                "#A91554") # Prolif
 
-DimPlot(SO, 
-        reduction = "umap", 
+DimPlot(SO,
+        reduction = "umap",
+        pt.size = 0,
         cols = mycols.DCT) +
   scale_x_reverse()
 
 ggsave(
-  "F3D.tiff",
-  device = "tiff",
-  plot = last_plot(),
-  width = 4,
-  height = 3,
-  units = c("in"),
-  dpi = 700,
-  compression = 'lzw'
-)
-
-# Fig.3E ---------------------------------------------------------------------------------------
-SO <- PrepSCTFindMarkers(SO, verbose = TRUE)
-
-markers <- FindAllMarkers(SO, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_log2FC)
-top10 <- markers %>% group_by(cluster) %>% top_n(10, avg_log2FC)
-
-DotPlot(SO,
-        features = unique(top10$gene),
-        cols = c("light grey", "royal blue"),
-        dot.scale = 8,
-        dot.min = 0,
-        scale.max = 100,
-        scale.min = 0,
-        col.min = -2.5,
-        col.max = 2.5) + 
-  theme(axis.text.x = element_text(),
-        axis.title = element_blank(),
-        plot.title = element_text(hjust = 0.5)) +
-  RotatedAxis() +
-  scale_y_discrete(limits = c("Prolif", "DCT2", "DCT1"))
-
-ggsave(
-  "F3E.tiff",
-  device = "tiff",
-  plot = last_plot(),
-  width = 10,
-  height = 3,
-  units = c("in"),
-  dpi = 700,
-  compression = 'lzw'
-)
-
-# Fig.3F ---------------------------------------------------------------------------------------
-FeaturePlot(SO,
-            features = c("DCT1_score1")) +
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 9, name = "RdYlBu"))) &
-  scale_x_reverse()
-
-ggsave(
-  "F3F.tiff",
-  device = "tiff",
-  plot = last_plot(),
-  width = 5,
-  height = 3,
-  units = c("in"),
-  dpi = 700,
-  compression = 'lzw'
-)
-
-# Fig.3G ---------------------------------------------------------------------------------------
-FeaturePlot(SO,
-            features = c("DCT2_score1")) +
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 9, name = "RdYlBu"))) &
-  scale_x_reverse()
-
-ggsave(
-  "F3G.tiff",
-  device = "tiff",
-  plot = last_plot(),
-  width = 5,
-  height = 3,
-  units = c("in"),
-  dpi = 700,
-  compression = 'lzw'
-)
-
-# Fig.3H ---------------------------------------------------------------------------------------
-# Covert Seurat object to a CellDataset object
-cds <- as.cell_data_set(SO)
-cds <- cluster_cells(cds = cds, reduction_method = "UMAP")
-
-# find all possible partitions
-all_partitions <- unique(cds@clusters$UMAP$partitions)
-all_partitions <- all_partitions[all_partitions != "1"]
-
-# set all partitions to 1
-cds@clusters$UMAP$partitions[cds@clusters$UMAP$partitions %in% all_partitions] <- "1"
-
-# set all cells to one partition, otherwise it won't show the trajectory across partitions. Set use_partition to FALSE as below.
-cds <- learn_graph(cds, use_partition = F)
-
-# Specify root cells programmatically
-get_earliest_principal_node <- function(cds, time_bin="DCT"){ # set the time_bin as 'DCT' so that all cells are from the same bin, no bias.
-  cell_ids <- which(colData(cds)[, "class"] == time_bin)
-  
-  closest_vertex <-
-    cds@principal_graph_aux[["UMAP"]]$pr_graph_cell_proj_closest_vertex
-  closest_vertex <- as.matrix(closest_vertex[colnames(cds), ])
-  root_pr_nodes <-
-    igraph::V(principal_graph(cds)[["UMAP"]])$name[as.numeric(names
-                                                              (which.max(table(closest_vertex[cell_ids,]))))]
-  
-  root_pr_nodes
-}
-
-#Passing the programatically selected root node to order_cells() via the root_pr_node argument yields:
-cds <- order_cells(cds, root_pr_nodes=get_earliest_principal_node(cds))
-
-plot_cells(cds = cds,
-           color_cells_by = "pseudotime",
-           show_trajectory_graph = TRUE,
-           trajectory_graph_color = "black",
-           trajectory_graph_segment_size = 1,
-           label_groups_by_cluster=FALSE,
-           label_cell_groups=FALSE,
-           label_leaves=FALSE,
-           label_branch_points=FALSE,
-           graph_label_size=3,
-           cell_size = 0.3) + scale_color_viridis_c() +
-  scale_x_reverse()
-
-ggsave(
-  "F3H.tiff",
+  "F7C_1.tiff",
   device = "tiff",
   plot = last_plot(),
   width = 4.5,
@@ -157,64 +34,216 @@ ggsave(
   compression = 'lzw'
 )
 
-# Fig.3I
-gene_list <- c("Erbb4", "Egf", "Trpm7", "Fgf13", "Col5a2", "Umod", "Ptgfr", "Stk32b", "Rtl4", "Abca13") # Top 10 in DCT1
-pt.matrix <- exprs(cds)[match(gene_list,rownames(rowData(cds))),order(pseudotime(cds))]
-pt.matrix <- t(apply(pt.matrix,1,function(x){smooth.spline(x,df=3)$y}))
-pt.matrix <- t(apply(pt.matrix,1,function(x){(x-mean(x))/sd(x)}))
-rownames(pt.matrix) <- gene_list
+DimPlot(SO,
+        reduction = "umap",
+        pt.size = 0,
+        group.by = "Rep") +
+  scale_x_reverse()
 
-tiff("F3I.tiff",
-     units="in",
-     width=7,
-     height=3,
-     res=700,
-     compression = 'lzw'
-     )
-
-Heatmap(
-  pt.matrix,
-  name                         = "z-score",
-  col                          = colorRamp2(seq(from=-2,to=2,length=11),rev(brewer.pal(11, "Spectral"))),
-  show_row_names               = TRUE,
-  show_column_names            = FALSE,
-  row_names_gp                 = gpar(fontsize = 6),
-  clustering_method_rows       = "ward.D2",
-  clustering_method_columns    = "ward.D2",
-  row_title_rot                = 0,
-  cluster_rows                 = TRUE,
-  cluster_row_slices           = FALSE,
-  cluster_columns              = FALSE)
-
-dev.off()
-
-# Fig.3J
-gene_list <- c("Slc8a1", "Arl15", "Calb1", "Slc2a9", "Phactr1", "Gls", "S100g", "Kl", "Klk1", "Egfem1")
-pt.matrix <- exprs(cds)[match(gene_list,rownames(rowData(cds))),order(pseudotime(cds))]
-pt.matrix <- t(apply(pt.matrix,1,function(x){smooth.spline(x,df=3)$y}))
-pt.matrix <- t(apply(pt.matrix,1,function(x){(x-mean(x))/sd(x)}))
-rownames(pt.matrix) <- gene_list
-
-tiff("F3J.tiff",
-     units="in",
-     width=7,
-     height=3,
-     res=700,
-     compression = 'lzw'
+ggsave(
+  "F7C_2.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 4.5,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
 )
 
-Heatmap(
-  pt.matrix,
-  name                         = "z-score",
-  col                          = colorRamp2(seq(from=-2,to=2,length=11),rev(brewer.pal(11, "Spectral"))),
-  show_row_names               = TRUE,
-  show_column_names            = FALSE,
-  row_names_gp                 = gpar(fontsize = 6),
-  clustering_method_rows       = "ward.D2",
-  clustering_method_columns    = "ward.D2",
-  row_title_rot                = 0,
-  cluster_rows                 = TRUE,
-  cluster_row_slices           = FALSE,
-  cluster_columns              = FALSE)
+# Fig. 7D ------------------------------------------------------------------------------------------------
+VlnPlot(SO,
+        features = c("DCT1_score1"),
+        split = TRUE,
+        split.by = "Diet",
+        cols = c("white", "lightskyblue"),
+        flip = TRUE,
+        pt.size = 0) & 
+  theme(legend.position = "right",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = .8),
+        plot.title = element_text(hjust = 0.5)) &
+  stat_summary(fun = mean,
+               geom = "crossbar",
+               width = 0.5,
+               size = 0.3,
+               position = position_dodge(width = 0.5)) &
+  scale_x_discrete(limits=c("DCT1", "DCT2"))
 
-dev.off()
+ggsave(
+  "F7D_1.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 2.2,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+VlnPlot(SO,
+        features = c("DCT2_score1"),
+        split = TRUE,
+        split.by = "Diet",
+        cols = c("white", "lightskyblue"),
+        flip = TRUE,
+        pt.size = 0) & 
+  theme(legend.position = "right",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = .8),
+        plot.title = element_text(hjust = 0.5)) &
+  stat_summary(fun = mean,
+               geom = "crossbar",
+               width = 0.5,
+               size = 0.3,
+               position = position_dodge(width = 0.5)) &
+  scale_x_discrete(limits=c("DCT1", "DCT2"))
+
+ggsave(
+  "F7D_2.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 2.2,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+VlnPlot(SO,
+        features = c("ENaC_score1"),
+        split = TRUE,
+        split.by = "Diet",
+        cols = c("white", "lightskyblue"),
+        flip = TRUE,
+        pt.size = 0) & 
+  theme(legend.position = "right",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = .8),
+        plot.title = element_text(hjust = 0.5)) &
+  stat_summary(fun = mean,
+               geom = "crossbar",
+               width = 0.5,
+               size = 0.3,
+               position = position_dodge(width = 0.5)) &
+  scale_x_discrete(limits=c("DCT1", "DCT2"))
+
+ggsave(
+  "F7D_3.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 2.2,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+# Fig. 7E ------------------------------------------------------------------------------------------------
+FeaturePlot(SO,
+            features = c("DCT1_score1"),
+            split.by = "Diet") &
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 9, name = "RdYlBu"))) &
+  scale_x_reverse() &
+  NoAxes()
+
+ggsave(
+  "F7E_1.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 7.5,
+  height = 3,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+FeaturePlot(SO,
+            features = c("DCT2_score1"),
+            split.by = "Diet") &
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 9, name = "RdYlBu"))) &
+  scale_x_reverse() &
+  NoAxes()
+
+ggsave(
+  "F7E_2.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 7.5,
+  height = 3,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+# Fig. 7F-H ----------------------------------------------------------------------------------------------
+df <- FetchData(object = SO, vars = c("subclass.l1", "analysis", "Rep", "Diet", "DCT1_score1", "DCT2_score1", "ENaC_score1")) %>% rownames_to_column()
+df2 <- subset(df, subclass.l1 == c("DCT1", "DCT2"))
+df2$analysis <- factor(x = df2$analysis, levels = c("DCT1_NK", "DCT2_NK", "DCT1_KD", "DCT2_KD"))
+df$Diet <- factor(x = df$Diet, levels = c("NK", "KD"))
+
+# Fig. 7F ------------------------------------------------------------------------------------------------
+ggplot(df2, aes(x = DCT1_score1, group = analysis, fill = analysis)) + 
+  geom_density(adjust=1.5,
+               alpha=.6,
+               color = c("black")) +
+  scale_fill_manual(values = c("white", "lightgrey", "lightskyblue", "#4CC3FF"))+
+  theme_pubr(legend = "right") +
+  ylab("Probability Density") +
+  xlab("DCT1 Score") +
+  coord_flip()
+
+ggsave(
+  "F7F.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 4.5,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+# Fig. 7G ------------------------------------------------------------------------------------------------
+ggplot(df2, aes(x = DCT2_score1, group = analysis, fill = analysis)) + 
+  geom_density(adjust=1.5,
+               alpha=.6,
+               color = c("black")) +
+  scale_fill_manual(values = c("white", "lightgrey", "lightskyblue", "#4CC3FF"))+
+  theme_pubr(legend = "right") +
+  ylab("Probability Density") +
+  xlab("DCT2 Score") +
+  coord_flip()
+
+ggsave(
+  "F7G.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 4.5,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
+
+# Fig. 7H ------------------------------------------------------------------------------------------------
+ggplot(df2, aes(x = ENaC_score1, group = analysis, fill = analysis)) + 
+  geom_density(adjust=1.5,
+               alpha=.6,
+               color = c("black")) +
+  scale_fill_manual(values = c("white", "lightgrey", "lightskyblue", "#4CC3FF"))+
+  theme_pubr(legend = "right") +
+  ylab("Probability Density") +
+  xlab("ENaC Score") +
+  coord_flip()
+
+ggsave(
+  "F7H.tiff",
+  device = "tiff",
+  plot = last_plot(),
+  width = 4.5,
+  height = 3.5,
+  units = c("in"),
+  dpi = 700,
+  compression = 'lzw'
+)
